b37cc05dc43272f92bfd604d057ba4b90bcc69e1
imada kakeru <imada@fun.bio.keio.ac.jp>
2024-04-05 22:08:03 +0900

    add explain Panel

--- src/Box/Window.java
+++ src/Box/Window.java
@@ -17,6 +17,7 @@ public class Window extends JFrame {
         //mainPanelを作成、これが電卓の土台
         JPanel mainPanel = new JPanel();
         this.output = new JTextField();
+        this.explain = new JTextField();
         this.oneGatya = new JButton("一連ガチャ");
         this.tenGatya = new JButton("10連ガチャ");
 
@@ -27,18 +28,18 @@ public class Window extends JFrame {
         output.setText("Lets play Gatya !!!");
         output.setColumns(20);
 
-//        explain.setMaximumSize(new Dimension(100,100));
-//        explain.setFont(new Font("Monospaced",Font.BOLD,27));
-//        explain.setDisabledTextColor(new Color(255,0,0));
-//        explain.setText("This monster is:");
-//        explain.setColumns(20);
+        explain.setMaximumSize(new Dimension(100,100));
+        explain.setFont(new Font("Monospaced",Font.BOLD,27));
+        explain.setDisabledTextColor(new Color(255,0,0));
+        explain.setText("This monster is:");
+        explain.setColumns(60);
 
         oneGatya.setFont(new Font("Monospaced", Font.BOLD,22));
         tenGatya.setFont(new Font("Monospaced", Font.BOLD,22));
 
 
         mainPanel.add(output);
-//        mainPanel.add(explain);
+        mainPanel.add(explain);
 
         this.add(mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
